6afb080f5c38fc67fce5c50efc2e9f49b5ded83c
Hisyam26 <hishisyam80@gmail.com>
2021-10-05 10:52:22 +0700

    feat[h] splash-screen-layout

---
+++ app/src/main/java/com/example/newsapps/Splash_screen.java
@@ -0,0 +1,27 @@
+package com.example.newsapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(Splash_screen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        }, 2000);
+    }
+}
